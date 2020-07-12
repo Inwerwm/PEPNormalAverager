eeea25545f3b21a795f2f158b63139b3add34aef
Inwerwm <saclainvervm@gmail.com>
2020-07-12 14:00:41 +0900

    処理の実装

--- NormalAverager/Main.cs
+++ NormalAverager/Main.cs
@@ -6,6 +6,8 @@ using System.Windows.Forms;
 using PEPlugin;
 using PEPlugin.Pmx;
 using PEPExtensions;
+using KdTree;
+using PEPlugin.SDX;
 
 namespace NormalAverager
 {
@@ -19,7 +21,7 @@ namespace NormalAverager
         {
             get
             {
-                return "プラグイン名";
+                return "選択頂点内の同位置頂点の法線を平均化";
             }
         }
 
@@ -35,7 +37,7 @@ namespace NormalAverager
         {
             get
             {
-                return "プラグイン説明";
+                return "選択頂点内の同位置頂点の法線を平均化";
             }
         }
 
@@ -44,7 +46,7 @@ namespace NormalAverager
             get
             {
                 // boot時実行, プラグインメニューへの登録, メニュー登録名
-                return new PEPluginOption(false, true, "プラグイン名");
+                return new PEPluginOption(false, true, "選択頂点内の同位置頂点の法線を平均化");
             }
         }
 
@@ -52,7 +54,25 @@ namespace NormalAverager
         {
             try
             {
+                var pmx = args.Host.Connector.Pmx.GetCurrentState();
+                var selectedVertices = args.Host.Connector.View.PmxView.GetSelectedVertexIndices().Select(i => pmx.Vertex[i]);
 
+                var tree = new KdTree<float, IPXVertex>(3, new KdTree.Math.FloatMath(), AddDuplicateBehavior.List);
+                foreach (var v in selectedVertices)
+                {
+                    tree.Add(v.Position.ToArray(), v);
+                }
+                foreach (var n in tree.DuplicatedNodes)
+                {
+                    var sum = n.Duplicate.Aggregate(new V3(0, 0, 0), (a, v) => a + v.Normal);
+
+                    foreach (var v in n.Duplicate)
+                    {
+                        v.Normal = sum / n.Duplicate.Count;
+                    }
+                }
+
+                Utility.Update(args.Host.Connector, pmx, PmxUpdateObject.Vertex);
             }
             catch (Exception ex)
             {
